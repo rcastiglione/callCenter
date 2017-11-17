965238872db3b6e2df2fa2abd93115ba4febebb9
Ricardo Castiglione <ricardo.castiglione@gmail.com>
2017-11-17 12:50:34 -0300

    first commit

---
+++ src/main/java/ar/com/almundo/callcenter/App.java
@@ -0,0 +1,13 @@
+package ar.com.almundo.callcenter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
